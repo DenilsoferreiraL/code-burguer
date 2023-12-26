584de713f927966523f55634ad20af9116464f1b
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2023-12-26 08:25:14 -0300

    chore: User Model Configuration

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import { v4 } from 'uuid'
+import User from './app/controllers/models/User'
 
 const routes = new Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Server Started " });
+routes.get('/', async (request, response) => {
+
+  const user = await User.create({
+    id: v4(),
+    name: 'Denilso',
+    email: 'denilso.7723@gmail.com',
+    password_hash: '123456',
+  })
+  return response.json(user);
+  
 });
 export default routes;
