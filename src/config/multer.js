cb3248fb5342f070aebe77d67c74a31ab9f0c5c6
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-01-02 11:44:23 -0300

    feat: Uploading images with Multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,3 +3,11 @@ import { v4 } from 'uuid'
 import { extname, resolve } from 'path'
 
 
+export default {
+    storage: multer.diskStorage({
+        destination: resolve(__dirname, '..', '..', 'uploads'),
+        filename: (request, file, callback) => {
+            return callback(null, v4() + extname(file.originalname))
+        }
+    })
+}
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,18 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
+import multer from 'multer';
+import multerconfig from './config/multer'
 
 const routes = new Router();
+
+const uploads = multer(multerconfig)
 
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
 
-routes.post('/products', ProductController.store)
+routes.post('/products', uploads.single('file'), ProductController.store)
 
 export default routes;
 
